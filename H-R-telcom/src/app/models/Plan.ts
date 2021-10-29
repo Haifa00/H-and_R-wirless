6acd5c16a1af94f77b177e271b413f460aa6fb5b
Work <work@omars-MacBook-Pro.local>
2021-10-28 21:26:17 -0500

    add the models to the folder and changing navbar style

--- H-R-telcom/src/app/models/Plan.ts
+++ H-R-telcom/src/app/models/Plan.ts
@@ -1,18 +1,21 @@
+import { Lines } from "./Lines";
+
 // Define the Plan object by the attribute in the database table
 export class Plan{
 
     planId?: number;// the id is generated from the database so we wont always have it?
-    numberOfDevices: number;
-    planCost: number;
-    callMinutes: number;
-    textAmount: number;
-    internetData: number;
+    name: string;
+    minLines:number;
+    description: string;
+    pricePerLine: number;
+    line:Lines[];
 
-    constructor(numberOfDevices: number, planCost: number, callMinutes: number, textAmount: number, internetData: number){
-        this.numberOfDevices = numberOfDevices;
-        this.planCost = planCost;
-        this.callMinutes = callMinutes;
-        this.textAmount = textAmount;
-        this.internetData = internetData;
+    constructor(name: string, minLines: number, description: string, pricePerLine: number,line:Lines[]){
+        this.name = name;
+        this.minLines = minLines;
+        this.description = description;
+        this.pricePerLine = pricePerLine;
+        this.line=line;
+    
     }
 }
